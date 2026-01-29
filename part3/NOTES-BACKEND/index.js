92036cc401ada13bcf7dc9aeb8f832d2767f7343
Katy Hatch <katyhatch@hotmail.com>
2026-01-29 13:48:59 +0000

    Updated code based on exercise code

--- part3/NOTES-BACKEND/index.js
+++ part3/NOTES-BACKEND/index.js
@@ -1,31 +1,15 @@
 // imprts nodes built in web server module
 // const http = require('http');
-
+require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
+const Note = require('./models/note');
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-];
+let notes = [];
 // return hardcoded data in json format to any request made to the server
 // request contains all info about http request
 // response contains methods used to send response back to client
@@ -35,18 +19,16 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
-});
+  Note.find({}).then(notes => {
+  response.json(notes)
+  })
+}); 
+
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  const note = notes.find(note => note.id === id);
-  //   checks if note exists and sends it as json response
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id;
@@ -70,16 +52,15 @@ app.post('/api/notes', (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     id: generateId()
-  };
+  });
 
-  notes = notes.concat(note);
-
-  console.log(note);
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote);
+  });
 });
 
 // creates server that responds with "Hello Full Stack!" to all requests
@@ -90,7 +71,7 @@ app.post('/api/notes', (request, response) => {
 // });
 
 // binding the http server to the app listening for requests sent to port 3001
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
