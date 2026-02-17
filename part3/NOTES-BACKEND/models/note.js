b86afb0b15224f42e4ce118349df7e9847066b05
Katy Hatch <katyhatch@hotmail.com>
2026-02-17 16:19:09 +0000

    Exercises 3.13 - 3.14 Amending the backend to fetch the data from database

--- part3/NOTES-BACKEND/models/note.js
+++ part3/NOTES-BACKEND/models/note.js
@@ -27,4 +27,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Note', noteSchema);;
+module.exports = mongoose.model('Note', noteSchema);
--- part3/Phonebook-Backend/index.js
+++ part3/Phonebook-Backend/index.js
@@ -1,40 +1,18 @@
+require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
-
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+// const morgan = require('morgan');
+const Person = require('./models/person');
 
 const app = express();
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) ;
-
 app.use(express.json());
 app.use(express.static('dist'))
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-];
+let persons = [];
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
 
 app.get('/info', (request, response) => {
@@ -78,21 +56,25 @@ app.post('/api/persons', (request, response) => {
       error: 'name must be unique'
     });
   }
-
-  const person = {
-    id: generateId(),
+  if(persons.find(p => p.number === body.number)) {
+    return response.status(400).json({
+      error: 'number must be unique'
+    });
+  }
+   
+  const person = new Person({
     name: body.name,
-    number: body.number
-  };
+    number: body.number,
+    id: generateId()
+  });
 
-  persons = persons.concat(person);
-
-  console.log(persons);
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  });
 
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
